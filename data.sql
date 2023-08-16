2cadfd1c3a54ecc3dbd493a1191c3446070532e8
manzitresor <83731517+manzitresor@users.noreply.github.com>
2023-08-16 13:27:21 +0200

    Add INSERT DATA

--- data.sql
+++ data.sql
@@ -1,5 +1,7 @@
 /* Populate database with sample data. */
 
+-- Animals Table
+
 INSERT INTO animals (name,date_of_birth,escape_attempts,neutered,weight_kg)VALUES ('Agumon', '2020-03-02', 0, true, 10.23);
 INSERT INTO animals (name,date_of_birth,escape_attempts,neutered,weight_kg)VALUES ('Gabumon', '2018-11-15', 2, true, 8.0);
 INSERT INTO animals (name,date_of_birth,escape_attempts,neutered,weight_kg)VALUES ('Pikachu', '2021-01-07', 1, false, 15.04);
@@ -11,4 +13,17 @@ INSERT INTO animals(name,date_of_birth,escape_attempts,neutered,weight_kg) VALUE
 INSERT INTO animals(name,date_of_birth,escape_attempts,neutered,weight_kg) VALUES('Boarmon','2005-06-07',7,true,20.4);
 INSERT INTO animals(name,date_of_birth,escape_attempts,neutered,weight_kg) VALUES('Blossom','1998-10-13',3,true,17);
 INSERT INTO animals(name,date_of_birth,escape_attempts,neutered,weight_kg) VALUES('Ditto','2022-05-14',4,true,22.0);
+
+-- Owners Table
+INSERT INTO owners(full_name,age) VALUES('Sam Smith',34);
+INSERT INTO owners(full_name,age) VALUES('Jennifer Orwell',19);
+INSERT INTO owners(full_name,age) VALUES('Bob',45);
+INSERT INTO owners(full_name,age) VALUES('Melody Pond',77);
+INSERT INTO owners(full_name,age) VALUES('Dean Winchester',14);
+INSERT INTO owners(full_name,age) VALUES('Jodie Whittaker',38);
+
+-- Species Table
+
+INSERT INTO species(name) VALUES('Pokemon');
+INSERT INTO species(name) VALUES('Digimon');
 
--- queries.sql
+++ queries.sql
@@ -42,3 +42,7 @@ SELECT *FROM animals;
  SELECT neutered, AVG(escape_attempts) FROM animals GROUP BY neutered;
  SELECT species, MAX(weight_kg), MIN(weight_kg) FROM animals GROUP BY species;
  SELECT species, AVG(escape_attempts) FROM animals WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-12-31' GROUP BY species;
+
+
+ UPDATE animals SET species_id=(SELECT id FROM species WHERE name='Digimon') WHERE name LIKE '%mon';
+ UPDATE animals SET species_id=(SELECT id FROM species WHERE name='Pokemon') WHERE species_id IS NULL;
